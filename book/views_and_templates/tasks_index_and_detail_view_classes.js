b9baeb282bb4a3a828c8c6500872a80028298fce
Jason Morrison <jmorrison@thoughtbot.com>
2011-07-28 15:06:09 -0400

    Update cleaning_up to motivate swapping router / leave

--- book/views_and_templates/tasks_index_and_detail_view_classes.js
+++ book/views_and_templates/tasks_index_and_detail_view_classes.js
@@ -4,6 +4,26 @@ var TasksIndex = Backbone.View.extend({
   id: 'tasks',
 
   initialize: function() {
+    this.tasks = this.options.tasks;
+
+    _.bindAll(this, "render");
+    TaskApp.tasks.bind("change", this.render);
+    TaskApp.tasks.bind("add", this.render);
+  },
+
+  render: function() {
+    $(this.el).html(this.template({tasks: this.tasks}));
+  }
+});
+
+var TasksDetail = Backbone.View.extend({
+  template: JST['tasks/tasks_detail'],
+  tagName: 'section',
+  id: 'task',
+
+  initialize: function() {
+    this.model = this.options.model;
+
     _.bindAll(this, "render");
     TaskApp.tasks.bind("change", this.render);
     TaskApp.tasks.bind("add", this.render);
@@ -11,21 +31,6 @@ var TasksIndex = Backbone.View.extend({
 
   render: function() {
     $(this.el).html(this.template({task: this.model}));
-  }
-});
-
-var TasksDetail = Backbone.View.extend({
-  tagName: 'section',
-  id: 'tasks',
-
-  initialize: function() {
-    _.bindAll(this, "render", "renderCompletedTasks", "renderOverdueTasks");
-    TaskApp.tasks.bind("change", this.render);
-    TaskApp.tasks.bind("add", this.render);
-  },
-
-  render: function() {
-    $(this.el).html(JST['tasks/task_detail']({task: this.model}));
   }
 });
 
