0f90cd5958e1c2c8581ec96bdf58359a40248dc4
Jason Morrison <jmorrison@thoughtbot.com>
2011-09-09 12:08:12 -0400

    Update refactored backbone_sync.rb

--- book/models_and_collections/backbone_sync.rb
+++ book/models_and_collections/backbone_sync.rb
@@ -1,19 +1,20 @@
 module BackboneSync
   module Rails
     module Faye
+      mattr_accessor :root_address
+      self.root_address = 'http://localhost:9292'
 
-      # module to mix into your ActiveModel::Observer
       module Observer
         def after_update(model)
-          Event.new(model, :update).broadcast
+          Event.new(model, :update).publish
         end
 
         def after_create(model)
-          Event.new(model, :create).broadcast
+          Event.new(model, :create).publish
         end
 
         def after_destroy(model)
-          Event.new(model, :destroy).broadcast
+          Event.new(model, :destroy).publish
         end
       end
 
@@ -24,30 +25,7 @@ module BackboneSync
         end
 
         def broadcast
-          Message.new(channel, data).send
-        end
-
-        private
-
-        def channel
-          "/sync/#{@model.class.table_name}"
-        end
-
-        def data
-          { @event => { @model.id => @model.as_json } }
-        end
-      end
-
-      # To publish from outside of an `EM.run {}` loop:
-      # http://groups.google.com/group/faye-users/browse_thread/thread/ae6e2a1cc37b3b07
-      class Message
-        def initialize(channel, data)
-          @channel = channel
-          @data = data
-        end
-
-        def send
-          Net::HTTP.post_form(uri, :message => payload) 
+          Net::HTTP.post_form(uri, :message => message) 
         end
 
         private
@@ -56,8 +34,17 @@ module BackboneSync
           URI.parse("#{BackboneSync::Rails::Faye.root_address}/faye")
         end
 
-        def payload
-          {:channel => @channel, :data => @data}.to_json
+        def message
+          { :channel => channel,
+            :data => data          }.to_json
+        end
+
+        def channel
+          "/sync/#{@model.class.table_name}"
+        end
+
+        def data
+          { @event => { @model.id => @model.as_json } }
         end
       end
     end
