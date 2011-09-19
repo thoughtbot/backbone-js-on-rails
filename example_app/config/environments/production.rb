a442d119eae0a636c57d8351a2751fb4c32f5c1b
bobz <bobz@bobz.in>
2011-09-19 16:46:10 -0400

    Update to Rails 3.1 final

--- example_app/config/environments/production.rb
+++ example_app/config/environments/production.rb
@@ -14,10 +14,17 @@ ExampleApp::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Specify the default JavaScript compressor
-  config.assets.js_compressor  = :uglifier
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  # Defaults to Rails.root.join("public/assets")
+  # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # (comment out if your front-end server doesn't support this)
   config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
 
