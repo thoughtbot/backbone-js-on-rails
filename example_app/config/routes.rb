c15af1353e435e68979ba8b722e49a296521c2ef
Jason Morrison <jmorrison@thoughtbot.com>
2011-09-22 17:00:43 -0400

    Example app: Add jasminerice to use 3.1 asset pipeline

--- example_app/config/routes.rb
+++ example_app/config/routes.rb
@@ -1,4 +1,8 @@
 ExampleApp::Application.routes.draw do
   resources :tasks
   root :to => 'tasks#index'
+
+  if ["development", "test"].include? Rails.env
+    mount Jasminerice::Engine => "/jasmine"
+  end
 end
