b8cfec47ee58c1f712cc4e84eff777be470d09e4
Jason Morrison <jmorrison@thoughtbot.com>
2011-11-03 11:25:22 -0400

    Install Clearance

--- example_app/db/schema.rb
+++ example_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111016183722) do
+ActiveRecord::Schema.define(:version => 20111103152415) do
 
   create_table "tasks", :force => true do |t|
     t.string   "title"
@@ -19,5 +19,18 @@ ActiveRecord::Schema.define(:version => 20111016183722) do
     t.datetime "updated_at"
     t.boolean  "complete",   :default => false, :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "encrypted_password", :limit => 128
+    t.string   "salt",               :limit => 128
+    t.string   "confirmation_token", :limit => 128
+    t.string   "remember_token",     :limit => 128
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
