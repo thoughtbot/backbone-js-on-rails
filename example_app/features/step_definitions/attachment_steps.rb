e5bb289e88418119fb4c46e4ea1a3180d45222d7
Jason Morrison <jmorrison@thoughtbot.com>
2011-11-09 14:38:41 -0500

    fix feature

--- example_app/features/step_definitions/attachment_steps.rb
+++ example_app/features/step_definitions/attachment_steps.rb
@@ -1,32 +1,37 @@
 When /^I attach "([^"]*)" to the "([^"]*)" task$/ do |path_to_upload, task_name|
   visit root_path
-  details_link = page.find(:xpath, "//label[contains(text(),'#{task_name}')]/following-sibling::a")
-  details_link.click
+  find_task_details_link(task_name).click
   attach_file "Attach a file to upload", File.expand_path(path_to_upload)
   click_button "Upload attachment"
 end
 
 Then /^I should see "([^"]*)" attached to the "([^"]*)" task$/ do |attachment_filename, task_name|
-  visit root_path
-  click_link task_name
   page.should have_content("Attached: #{attachment_filename}")
   page.should have_css("img[src*='#{attachment_filename}']")
 end
 
 Then /^I should see no attachments on the "([^"]*)" task$/ do |task_name|
   visit root_path
-  click_link task_name
+  find_task_details_link(task_name).click
   page.should have_no_content("Attached:")
 end
 
 When /^I remove the "([^"]*)" attachment from the "([^"]*)" task$/ do |attachment_filename, task_name|
   visit root_path
-  click_link task_name
+  find_task_details_link(task_name).click
   pending # express the regexp above with the code you wish you had
 end
 
 Then /^I should not see "([^"]*)" attached to the "([^"]*)" task$/ do |attachment_filename, task_name|
   visit root_path
-  click_link task_name
+  find_task_details_link(task_name).click
   pending # express the regexp above with the code you wish you had
 end
+
+module AttachmentStepsHelpers
+  def find_task_details_link(task_name)
+    page.find(:xpath, "//label[contains(text(),'#{task_name}')]/following-sibling::a")
+  end
+end
+
+World(AttachmentStepsHelpers)
