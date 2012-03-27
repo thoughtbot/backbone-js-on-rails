255959e8acd2f7f6fdd548f721f762db0384e8fc
Jason Morrison <jason.p.morrison@gmail.com>
2012-03-27 23:43:40 +0700

    Use vanilla jasmine-jquery-1.3.1.js, not the Jasminerice modified version

--- example_app/spec/javascripts/support/jasmine-jquery-1.3.1.js
+++ example_app/spec/javascripts/support/jasmine-jquery-1.3.1.js
@@ -29,9 +29,7 @@ jasmine.getFixtures = function() {
 jasmine.Fixtures = function() {
   this.containerId = 'jasmine-fixtures';
   this.fixturesCache_ = {};
-  // Changed in jasminerice to support the normal path
-  //this.fixturesPath = 'spec/javascripts/fixtures';
-  this.fixturesPath = 'jasmine/fixtures';
+  this.fixturesPath = 'spec/javascripts/fixtures';
 };
 
 jasmine.Fixtures.prototype.set = function(html) {
@@ -83,7 +81,7 @@ jasmine.Fixtures.prototype.createContainer_ = function(html) {
   jQuery('body').append(container);
 };
 
-jasmine.Fixtures.prototype.getFixtureHtml_ = function(url) {
+jasmine.Fixtures.prototype.getFixtureHtml_ = function(url) {  
   if (typeof this.fixturesCache_[url] == 'undefined') {
     this.loadFixtureIntoCache_(url);
   }
@@ -225,7 +223,7 @@ jasmine.JQuery.matchersClass = {};
       var events = this.actual.data("events");
       return events && events[eventName].length > 0;
     },
-
+    
     // tests the existence of a specific event binding + handler
     toHandleWith: function(eventName, eventHandler) {
       var stack = this.actual.data("events")[eventName];
