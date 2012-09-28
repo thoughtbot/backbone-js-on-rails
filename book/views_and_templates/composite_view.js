c5a35b595c4d3ad9e3df2c52764059d43aaf3c49
Jason Morrison <jason.p.morrison@gmail.com>
2012-09-28 16:42:53 +0100

    Update to backbone-support 0.3.0 with bindTo

--- book/views_and_templates/composite_view.js
+++ book/views_and_templates/composite_view.js
@@ -1,14 +1,28 @@
 Support.CompositeView = function(options) {
   this.children = _([]);
+  this.bindings = _([]);
   Backbone.View.apply(this, [options]);
 };
 
 _.extend(Support.CompositeView.prototype, Backbone.View.prototype, {
   leave: function() {
     this.unbind();
+    this.unbindFromAll();
     this.remove();
     this._leaveChildren();
     this._removeFromParent();
+  },
+
+  bindTo: function(source, event, callback) {
+    source.bind(event, callback, this);
+    this.bindings.push({ source: source, event: event, callback: callback });
+  },
+
+  unbindFromAll: function() {
+    this.bindings.each(function(binding) {
+      binding.source.unbind(binding.event, binding.callback);
+    });
+    this.bindings = _([]);
   },
 
   renderChild: function(view) {
@@ -16,15 +30,30 @@ _.extend(Support.CompositeView.prototype, Backbone.View.prototype, {
     this.children.push(view);
     view.parent = this;
   },
+  
+  renderChildInto: function(view, container) {
+    this.renderChild(view);
+    $(container).empty().append(view.el);
+  },
 
   appendChild: function(view) {
     this.renderChild(view);
     $(this.el).append(view.el);
   },
-
-  renderChildInto: function(view, container) {
+  
+  appendChildTo: function (view, container) {
     this.renderChild(view);
-    $(container).empty().append(view.el);
+    $(container).append(view.el);
+  },
+  
+  prependChild: function(view) {
+    this.renderChild(view);
+    $(this.el).prepend(view.el);
+  },
+  
+  prependChildTo: function (view, container) {
+    this.renderChild(view);
+    $(container).prepend(view.el);
   },
 
   _leaveChildren: function() {
